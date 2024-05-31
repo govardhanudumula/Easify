32d6cbd571698e19a32a7aac113d0a4b5e6c6730
Naveen Dharavatu <ndharavatu@digitalrealty.com>
2024-05-31 21:13:03 +0530

    user form

--- src/Components/Candidate/Form/Form.tsx
+++ src/Components/Candidate/Form/Form.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 class Form extends React.Component {
     render() {
         return (<>
-            <div>sjdlvjsdj</div>
+            <div>User form</div>
         </>)
 
     }
